4ab919a6dfee116cfa84053f348721d0b94dbe64
watayouxiang <watayouxiang@qq.com>
2016-07-17 13:56:48 +0800

    初始化项目

---
+++ bsbdj/bsbdj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bsbdj
+//
+//  Created by 王涛 on 16/7/17.
+//  Copyright © 2016年 wata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
