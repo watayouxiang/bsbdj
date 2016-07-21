4bac072ec4b528e745b12d196a1a51898cb84851
watayouxiang <watayouxiang@qq.com>
2016-07-21 09:25:19 +0800

    加载 "精华" - "段子" 分页的内容

--- bsbdj/bsbdj/Classes/Essence/Controller/ttWordVc.m
+++ bsbdj/bsbdj/Classes/Essence/Controller/ttWordVc.m
@@ -7,9 +7,12 @@
 //
 
 #import "ttWordVc.h"
+#import "AFNetworking.h"
+#import "UIImageView+WebCache.h"
 
 @interface ttWordVc ()
-
+/** 帖子数据 */
+@property (nonatomic, strong) NSArray *topics;
 @end
 
 @implementation ttWordVc
@@ -17,12 +20,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @"29";
+    
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        self.topics = responseObject[@"list"];
+        [self.tableView reloadData];
+        [responseObject writeToFile:@"/Users/TaoWang/Desktop/tiezi.plist" atomically:YES];
+        
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
+    
     
 }
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 50;
+    return self.topics.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -33,10 +53,14 @@
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
-//        cell.backgroundColor = [UIColor blueColor];
+        //cell.backgroundColor = [UIColor blueColor];
     }
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@----%zd", [self class], indexPath.row];
+    NSDictionary *topic = self.topics[indexPath.row];
+    cell.textLabel.text = topic[@"name"];
+    cell.detailTextLabel.text = topic[@"text"];
+    
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:topic[@"profile_image"]] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
     
     return cell;
 }
