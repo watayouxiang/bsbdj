5e90c7220b12018f82e4f2e9611c8a623611147e
watayouxiang <watayouxiang@qq.com>
2016-07-22 15:29:50 +0800

    完成(100%): 我的关注-立即登录注册

--- bsbdj/bsbdj/Classes/FriendTrends/Controller/ttLoginRegisterVc.m
+++ bsbdj/bsbdj/Classes/FriendTrends/Controller/ttLoginRegisterVc.m
@@ -9,6 +9,7 @@
 #import "ttLoginRegisterVc.h"
 
 @interface ttLoginRegisterVc ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
 
 @end
 
@@ -16,20 +17,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  登录或注册
+ */
+- (IBAction)loginOrRegisterBtn:(id)sender {
+    UIButton *button = (UIButton *)sender;
+    
+    // 退出键盘
+    [self.view endEditing:YES];
+    
+    if (self.loginViewLeftMargin.constant == 0) { // 显示注册界面
+        self.loginViewLeftMargin.constant = - self.view.width;
+        button.selected = YES;
+    } else { // 显示登录界面
+        self.loginViewLeftMargin.constant = 0;
+        button.selected = NO;
+    }
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];//立即刷新布局
+    }];
 }
 
+/**
+ *  点击退出键盘
+ */
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+/**
+ *  返回
+ */
 - (IBAction)back:(UIButton *)sender {
+    
+    // 退出键盘
+    [self.view endEditing:YES];
+    
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 /**
- * 让当前控制器对应的状态栏是白色
+ *  让当前控制器对应的状态栏是白色
  */
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
